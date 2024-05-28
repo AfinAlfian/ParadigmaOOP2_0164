e36f7aad1e6d83d4d0381b91d3d4203102ac55c9
AfinAlfian <alfinalfiansyah528@gmail.com>
2024-05-28 10:32:00 +0700

    deklarasi class baseClass

---
+++ noOverriding/noOverriding.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+//class parent
+//tambahan untuk  final sesudah nama class
+//untuk mencegah adanya overriding
+
+class baseClass {
+
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
