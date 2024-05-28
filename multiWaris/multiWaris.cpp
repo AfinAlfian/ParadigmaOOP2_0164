f101f58d2f6ab0137eeaa0780dda4e636dd63aeb
AfinAlfian <alfinalfiansyah528@gmail.com>
2024-05-28 11:31:15 +0700

    menambahkan class budi

--- multiWaris/multiWaris.cpp
+++ multiWaris/multiWaris.cpp
@@ -28,6 +28,18 @@ public:
         cout << "pelajar dibuat\n" << endl;
     }
 };
+
+class budi : public pekerja, public pelajar {
+public:
+    budi(int pUmur) :
+        pekerja(pUmur),
+        pelajar(pUmur),
+        orang(pUmur)    //hal ini dapat dlakukan jika menggunakan virtual ...
+    {
+        cout << "Budi dibuat\n" << endl;
+    }
+};
+
 int main()
 {
     std::cout << "Hello World!\n";
