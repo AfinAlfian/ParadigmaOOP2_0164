6e1ef070f5ed12312395ea6fd061ba22677a12d4
AfinAlfian <alfinalfiansyah528@gmail.com>
2024-05-28 10:36:40 +0700

    menambahkan fungsi override pengenalan

--- noOverriding/noOverriding.cpp
+++ noOverriding/noOverriding.cpp
@@ -13,7 +13,11 @@ public:
 };
 
 class  derivedClass : public baseClass {
-
+public:
+    void perkenalan() 
+    {
+        cout << "Halo saya Function dari derived class";
+    }
 };
 
 int main()
